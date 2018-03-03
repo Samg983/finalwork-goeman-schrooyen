2ccab49846f27394064b36ef35b6252e5b292ca1
root <root@raspberrypi>
2018-03-03 11:18:03 +0100

    NEw commit

---
+++ src/main/java/be/finalwork/goemanschrooyen/model/Auto.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.finalwork.goemanschrooyen.model;
+
+/**
+ *
+ * @author pi
+ */
+public class Auto {
+        private int autoId;
+	private Double snelheid;
+	private Boolean schermAan;
+        private Double kmStand;
+
+    public int getAutoId() {
+        return autoId;
+    }
+
+    public void setAutoId(int autoId) {
+        this.autoId = autoId;
+    }
+
+    public Double getSnelheid() {
+        return snelheid;
+    }
+
+    public void setSnelheid(Double snelheid) {
+        this.snelheid = snelheid;
+    }
+
+    public Boolean getSchermAan() {
+        return schermAan;
+    }
+
+    public void setSchermAan(Boolean schermAan) {
+        this.schermAan = schermAan;
+    }
+
+    public Double getKmStand() {
+        return kmStand;
+    }
+
+    public void setKmStand(Double kmStand) {
+        this.kmStand = kmStand;
+    }
+
+	// Default constructor is nodig voor automatische omzetting van JSON naar
+	// object
+	public Auto() {
+
+	}
+	
+        @Override
+	public String toString(){
+		return "Auto nr: " + this.autoId;
+	}
+}
---
+++ src/main/java/be/finalwork/goemanschrooyen/webservice/Webservice.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.finalwork.goemanschrooyen.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+/**
+ *
+ * @author Maarten Heylen
+ */
+//@EnableAutoConfiguration
+
+@SpringBootApplication
+public class Webservice {
+        public static void main(String[] args) {
+        SpringApplication.run(Webservice.class, args);
+    }
+}

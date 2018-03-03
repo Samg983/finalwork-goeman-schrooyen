fef6be794970b9627b8538a748403686c65b4dfe
Sam <sam.goeman@hotmail.com>
2018-03-03 11:28:16 +0100

    Media

---
+++ src/main/java/be/finalwork/goemanschrooyen/model/Media.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.finalwork.goemanschrooyen.model;
+
+/**
+ *
+ * @author SamGoeman
+ */
+public class Media {
+    
+}

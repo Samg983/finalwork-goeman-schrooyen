7e6f72dcd6bda9fb95573f4ec808dbd803f0ae9a
root <root@raspberrypi>
2018-03-18 14:33:02 +0100

    Blink

--- src/main/java/be/finalwork/goemanschrooyen/webservice/AutoController.java
+++ src/main/java/be/finalwork/goemanschrooyen/webservice/AutoController.java
@@ -6,11 +6,14 @@
 package be.finalwork.goemanschrooyen.webservice;
 
 import be.finalwork.goemanschrooyen.dao.AutoDao;
+import be.finalwork.goemanschrooyen.dao.KlimaatDao;
 import be.finalwork.goemanschrooyen.model.Auto;
+import be.finalwork.goemanschrooyen.model.Klimaat;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,17 +52,60 @@ public class AutoController {
         @RequestMapping("/temp")
 	public void temp()
         {
+            System.out.println("Start temp");
+            //Klimaat klimaat = KlimaatDao.getKlimaatById(1);
+            
             
             try {
-              
                  I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
 
        
-                   I2CDevice device = i2c.getDevice(0b1001001);
-                    System.out.print(device);
-                 //int response = device.read(TSL2561_REG_ID);
-            } catch (UnsupportedBusNumberException e) {
+                  I2CDevice device = i2c.getDevice(0b1001000);
+                  /*device.write((byte) 0b1100011);
+                  System.out.println("written");
+                  Thread.sleep(500);
+                   int value = device.read(0b1001000);
+                    //Thread.sleep(800);
+                    System.out.println("value: " + value);
+                    
+                    
+                    klimaat.setTemperatuur(value);
+                    KlimaatDao.updateKlimaat(klimaat);*/
+              int i = 0;
+                do {
+                    
+                    int btn = device.read(2);
+                    int x = device.read(1);
+                    int y = device.read(0b00000000);
+                    
+                    /* if (x < 10){ 
+                        System.out.println("left: " + x);
+                    } else if (x > 250){
+                        System.out.println("right: " + x);
+                    }
+                    if (y < 10){
+                        System.out.println("down: " + y);
+                    } else if (y > 250){
+                        System.out.println("up: " + y);
+                    }
+                    if (btn == 0){
+                        System.out.println("Button: " + btn);
+                    }*/
+                    Thread.sleep(800);  
+                    System.out.println("x: " + x);
+                    System.out.println("y: " + y);
+                    System.out.println("Btn: " + btn);
+                    
+                   i++; 
+                } while(i < 100);
                 
+                 
+            } catch (IOException e) {
+                 System.out.println(e);
+            } catch (UnsupportedBusNumberException e) {
+                 System.out.println(e);
+            }catch (InterruptedException e) {
+                 System.out.println(e);
             }
 		
 

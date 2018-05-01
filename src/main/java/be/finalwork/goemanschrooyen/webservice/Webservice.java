b30d0865a3ac380c75e8c995ce5abe3a1e2e6cb4
Sam <sam.goeman@hotmail.com>
2018-05-01 13:20:03 +0200

    e

--- src/main/java/be/finalwork/goemanschrooyen/webservice/Webservice.java
+++ src/main/java/be/finalwork/goemanschrooyen/webservice/Webservice.java
@@ -9,6 +9,7 @@ import be.finalwork.goemanschrooyen.devices.HomeButton;
 import be.finalwork.goemanschrooyen.observers.MyObserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *
  * @author Maarten Heylen
@@ -17,11 +18,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Webservice {
-        public static void main(String[] args) {
-        
-       HomeButton button = new HomeButton();
-     
-       
-       SpringApplication.run(Webservice.class, args);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webservice.class, args);
     }
 }
